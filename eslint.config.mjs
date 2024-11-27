778a24452481887da7402547c98d1c057b8f1e58
refgd <j124519951@gmail.com>
2024-11-27 18:14:11 -0500

    添加黑木耳

---
+++ eslint.config.mjs
@@ -0,0 +1,38 @@
+import globals from "globals";
+import pluginJs from "@eslint/js";
+
+
+/** @type {import('eslint').Linter.Config[]} */
+export default [
+  {
+    files: ["**/*.js"], 
+    languageOptions: {
+      sourceType: "commonjs"
+    },
+  },
+  {
+    languageOptions: {
+      globals: {
+        ...globals.browser,
+        java: 'readonly',
+        Cartoon: 'readonly',
+        Inject_NetworkHelper: 'readonly',
+        Inject_PreferenceHelper: 'readonly',
+        Inject_WebViewHelperV2: 'readonly',
+        ArrayList: 'readonly',
+        JSLogUtils: 'readonly',
+        SourcePreference: 'readonly',
+        MainTab: 'readonly',
+        SubTab: 'readonly',
+        Pair: 'readonly',
+        PlayerInfo: 'readonly',
+        Jsoup: 'readonly',
+        makeCartoonCover: 'readonly',
+        makeCartoon: 'readonly',
+        PlayLine: 'readonly',
+        Episode: 'readonly',
+      }
+    }
+  },
+  pluginJs.configs.recommended,
+];
